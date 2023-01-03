e757c6cc4ca2aec5562720df7960b17e64a9b9cf
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-01-03 12:38:11 +0200

    Update ping.c

--- ping.c
+++ ping.c
@@ -1,221 +1,144 @@
-/*
- *  Communication and Computing Course Assigment 4:
- *  RAW Sockets and ICMP Protocol
- *  Copyright (C) 2022-2023  Roy Simanovich and Yuval Yurzdichinsky
- *  
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
+#include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <signal.h>
 #include <stdlib.h>
-#include <arpa/inet.h>
-#include <errno.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include "ping_func.h"
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/ip_icmp.h>
+#include <time.h>
+#include<errno.h>
+#include <errno.h>
 
-int main(int argc, char* argv[]) {
-    // Varibles setup
-    struct icmp icmphdr;
-    struct iphdr *iphdr_res;
-    struct icmphdr *icmphdr_res;
-    struct sockaddr_in dest_in;
-    struct timeval start, end;
+#define MSG_LEN 32
+#define ICMP_HDRLEN 8
 
-    socklen_t addr_len;
-    ssize_t bytes_received = 0;
-    size_t datalen;
+unsigned short checksum(void *b, int len)
+{	unsigned short *buf = b;
+	unsigned int sum=0;
+	unsigned short result;
 
-    int socketfd = INVALID_SOCKET;
+	for ( sum = 0; len > 1; len -= 2 )
+		sum += *buf++;
+	if ( len == 1 )
+		sum += *(unsigned char*)buf;
+	sum = (sum >> 16) + (sum & 0xFFFF);
+	sum += (sum >> 16);
+	result = ~sum;
+	return result;
+}
 
-    double pingPongTime = 0.0;
+int main(int argc, char *argv[])
+{
+    int sock = 0, len = 0, seq = 0;
 
-    char packet[IP_MAXPACKET], response[IP_MAXPACKET], data[ICMP_ECHO_MSG_LEN], responseAddr[INET_ADDRSTRLEN];
+    struct sockaddr_in addr_ping;
+    memset(&addr_ping, 0, sizeof(addr_ping));
 
-    // Prepare the data we're going to send.
-    for (int i = 0; i < ICMP_ECHO_MSG_LEN - 1; ++i)
+    struct icmphdr icmp;
+    memset(&icmp, 0, sizeof(icmp));
+
+    char buffer[IP_MAXPACKET] = {'\0'};
+    size_t buffer_len = sizeof(buffer);
+
+    struct timeval start , end; 
+    memset(&start , 0 , sizeof(start));
+    memset(&end , 0 , sizeof(end));
+
+    socklen_t addr_len = 0;
+
+    double time = 0.0;
+
+    struct iphdr *ip;
+    memset(&ip , 0 , sizeof(ip));
+
+    char packet[IP_MAXPACKET] = {0} , data[MSG_LEN] = {0};
+    size_t data_len = strlen(data) + 1;
+
+    for (size_t i = 0; i < MSG_LEN - 1; i++) //the message we sent
+    {
         data[i] = '1';
-
-    data[ICMP_ECHO_MSG_LEN - 1] = '\0';
-    datalen = (strlen(data) + 1);
-
-    // Check the arguments passed to the program and check IP validity.
-    checkArguments(argc, argv[1], &dest_in, &addr_len);
-
-    // Prepare the RAW socket.
-    socketfd = setupRawSocket(&icmphdr, getpid());
-
-    printf("PING %s: %ld data bytes\n", argv[1], datalen);
-
-    while(1)
-    {
-        // Prepare the ICMP ECHO packet.
-        preparePing(packet, &icmphdr, data, datalen);
-
-        // Calculate starting time.
-        gettimeofday(&start, NULL);
-
-        // Send the ICMP ECHO packet to the destination address.
-        sendICMPpacket(socketfd, packet, datalen, &dest_in, sizeof(dest_in));
-
-        // Wait and receive the ICMP ECHO REPLAY packet.
-        bytes_received = receiveICMPpacket(socketfd, response, sizeof(response), &dest_in, &addr_len);
-
-        // Calculate ending time.
-        gettimeofday(&end, NULL);
-
-        // Calculate the time it took to send and receive the packet
-        pingPongTime = ((end.tv_sec - start.tv_sec) * 1000) + (((double)end.tv_usec - start.tv_usec) / 1000);
-
-        // Extract the ICMP ECHO Replay headers via the IP header
-        iphdr_res = (struct iphdr *)response;
-        icmphdr_res = (struct icmphdr *)(response + iphdr_res->ihl*4);
-
-        inet_ntop(AF_INET, &(iphdr_res->saddr), responseAddr, INET_ADDRSTRLEN);
-
-        // Print the packet data (total length, source IP address, ICMP ECHO REPLAY sequance number, IP Time-To-Live and the calculated time).
-        printf("%ld bytes from %s: icmp_seq=%d ttl=%d time=%0.3lf ms\n", 
-        bytes_received, 
-        responseAddr, 
-        ntohs(icmphdr_res->un.echo.sequence),
-        iphdr_res->ttl, 
-        pingPongTime);
-
-        // Make the ping program sleep some time before sending another ICMP ECHO packet.
-        usleep(PING_WAIT_TIME);
     }
-
-    // Closing the RAW socket and finish.
-    close(socketfd);
-
-    return 0;
-}
-
-void checkArguments(int argc, char* argv, struct sockaddr_in* dest_in, socklen_t* addr_len) {
-    // We must have 2 arguments passed
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: ./ping <ip address>\n");
-        exit(1);
-    }
-
-    memset(dest_in, 0, sizeof(*dest_in));
-
-    // Try to convert the second agument to a binary IPv4 address
-    if (inet_pton(AF_INET, argv, &(dest_in->sin_addr)) <= 0)
-    {
-        fprintf(stderr, "Invalid IP Address\n");
-        exit(1);
-    }
-
-    dest_in->sin_family = AF_INET;
     
-    *addr_len = sizeof(*dest_in);
-}
+    data[MSG_LEN - 1] = '\0';
 
-int setupRawSocket(struct icmp *icmphdr, int id) {
-    int socketfd = INVALID_SOCKET;
 
-    if ((socketfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == INVALID_SOCKET)
+    if (argc != 2) // checking that the user has specified an IP address 
     {
-        fprintf(stderr, "To create a raw socket, the process needs to be run by root user.\n");
-        perror("socket");
-        exit(1);
+        printf("usage: ./partb <ip>\n");
+        exit(EXIT_FAILURE);
     }
 
-    icmphdr->icmp_type = ICMP_ECHO;
-    icmphdr->icmp_code = 0;
-    icmphdr->icmp_id = id;
-
-    return socketfd;
-}
-
-void preparePing(char *packet, struct icmp *icmphdr, char *data, size_t datalen) {
-    static uint16_t seq = 0;
-
-    bzero(packet, IP_MAXPACKET);
-
-    // Prepares the ICMP packet data
-    icmphdr->icmp_seq = htons(++seq);
-    icmphdr->icmp_cksum = 0;
-
-    memcpy((packet), icmphdr, ICMP_HDRLEN);
-    memcpy(packet + ICMP_HDRLEN, data, datalen);
-
-    icmphdr->icmp_cksum = calculate_checksum((unsigned short *)(packet), ICMP_HDRLEN + datalen);
-    memcpy((packet), icmphdr, ICMP_HDRLEN);
-}
-
-ssize_t sendICMPpacket(int socketfd, char* packet, int datalen, struct sockaddr_in *dest_in, socklen_t len) {
-    ssize_t bytes_sent = sendto(socketfd, packet, ICMP_HDRLEN + datalen, 0, (struct sockaddr *)dest_in, len);
-
-    if (bytes_sent == -1)
+    sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP); // creating an RAW socket for ICMP communication
+    if (sock <= 0) // checking if socket created
     {
-        perror("sendto");
-        exit(1);
+        perror("socket() failed");
+        close(sock);
+        exit(errno);
     }
 
-    return bytes_sent;
-}
-
-ssize_t receiveICMPpacket(int socketfd, void* response, int response_len, struct sockaddr_in *dest_in, socklen_t *len) {
-    ssize_t bytes_received = 0;
-
-    bzero(response, IP_MAXPACKET);
-
-    while ((bytes_received = recvfrom(socketfd, response, response_len, 0, (struct sockaddr *)dest_in, len)))
+    addr_ping.sin_family = AF_INET; // setting up the struct for ICMP communicationg
+    if(inet_aton(argv[1], &addr_ping.sin_addr) < 0)
     {
-        if (bytes_received == -1)
+        perror("inet_aton() failed");
+        exit(errno);
+    }
+
+    while (1)
+    {
+        memset(&icmp, 0, sizeof(icmp)); // setting up the struct and packet for ICMP communication
+        memset(&packet , 0 , sizeof(packet));
+        icmp.type = ICMP_ECHO;
+        icmp.un.echo.sequence = seq;
+        icmp.checksum = 0;
+
+        memcpy((packet), &icmp, ICMP_HDRLEN);
+        memcpy(packet + ICMP_HDRLEN, data, data_len);
+
+        icmp.checksum = checksum(&packet, sizeof(packet));
+        memcpy((packet), &icmp, ICMP_HDRLEN);
+
+        gettimeofday(&start , NULL); // starting counting ping-time
+        if (sendto(sock, &packet, ICMP_HDRLEN + data_len, 0, (struct sockaddr*)&addr_ping, sizeof(addr_ping)) < 0) // sending ICMP-ECHO-REQUEST
         {
-            perror("recvfrom");
-            exit(1);
+            perror("sendto() failed");
+            close(sock);
+            exit(errno);
         }
 
-        else if (bytes_received > 0)
-            break;
+        addr_len = sizeof(addr_ping); // receiving ICMP-ECHO-REPLEY
+        bzero(buffer, IP_MAXPACKET);
+        while ((len = recvfrom(sock, buffer, buffer_len, 0, (struct sockaddr *)&addr_ping, &addr_len)))
+        {
+            if (len == -1)
+            {
+                perror("recvfrom() failed");
+                close(sock);
+                exit(errno);
+            }
+
+            else if (len > 0){
+                break;
+            }
+        }
+
+        gettimeofday(&end , NULL); // ending counting ping-time
+
+        time = (end.tv_sec - start.tv_sec) * 1000.0 + (end.tv_usec - start.tv_usec) / 1000.0; // saving the time in mili-seconds
+        
+        ip = (struct iphdr*)buffer;
+
+        printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.2f ms\n", 
+            len, inet_ntoa(*(struct in_addr*)&ip->saddr), icmp.un.echo.sequence , ip->ttl, time);
+        
+        seq++;
+
+        sleep(1);
     }
 
-    return bytes_received;
-}
-
-unsigned short calculate_checksum(unsigned short *paddress, int len) {
-    int nleft = len, sum = 0;
-    unsigned short *w = paddress;
-    unsigned short answer = 0;
-
-    while (nleft > 1)
-    {
-        sum += *w++;
-        nleft -= 2;
-    }
-
-    if (nleft == 1)
-    {
-        *((unsigned char *)&answer) = *((unsigned char *)w);
-        sum += answer;
-    }
-
-    // add back carry outs from top 16 bits to low 16 bits
-    sum = (sum >> 16) + (sum & 0xffff); // add hi 16 to low 16
-    sum += (sum >> 16);                 // add carry
-    answer = ~sum;                      // truncate to 16 bits
-
-    return answer;
+    close(sock);
+    exit(EXIT_SUCCESS);
 }
